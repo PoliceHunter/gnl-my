da5150e4008f986b5feb1c50d7045661212ab0c4
Taisha Myrcella <tmyrcell@vo-j5.21-school.ru>
2019-10-14 17:57:34 +0300

    gnl22

--- get_next_line.c
+++ get_next_line.c
@@ -6,97 +6,112 @@
 /*   By: tmyrcell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 15:37:05 by tmyrcell          #+#    #+#             */
-/*   Updated: 2019/10/01 17:35:12 by tmyrcell         ###   ########.fr       */
+/*   Updated: 2019/10/14 17:55:14 by tmyrcell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <string.h>
 
-
-struct rest_data
+struct list_s	*push(struct list_s *parent, void *data)
 {
-	int fd;
-	char * rest;
+	struct list_s *new_node = (struct list_s *)malloc(sizeof(struct list_s));
+	new_node->data = data;
+	new_node->next = NULL;
+	new_node->parent = NULL;
+	new_node->parent = parent;
+	if (parent != NULL)
+		parent->next = new_node;
+	return (new_node);
 }
 
-char * pop_res(int fd, struct list_s * rest_cash)
+struct list_s	*push_to_end(struct list_s *node, void *data)
 {
-	rest_data * tmp;
-	char * result;
-	char * ptr;
+	if (node == NULL)
+	{
+		struct list_s *new_node = (struct list_s *)malloc(sizeof(struct list_s));
+		new_node->data = data;
+		new_node->next = NULL;
+		new_node->parent = NULL;
+		return (new_node);
+	}
+	while (node->next != NULL)
+		node = node->next;
+	return (push(node, data));
+}
+
+void	*pop(struct list_s *node)
+{
+	void	*data;
+	data = node->data;
+	if (node->parent != NULL)
+		node->parent->next = node->next;
+	if (node->next != NULL)
+		node->next->parent = node->parent;
+	free(node);
+	return (data);
+}
+
+char	pop_res(int fd, struct list_s *rest_cash)
+{
+	struct rest_data	*tmp;
+	char				*ptr;
+	char				*copy;
 
 	while (rest_cash != NULL)
 	{
-		tmp = (rest_data*)rest_cash->data;
+		tmp = rest_cash->data;
 		if (data->fd == fd)
 		{
 			tmp = pop(rest_cash);
-			if ((ptr = ft_strchr(tmp, '\n')))
+			if ((ptr = ft_strchr((const char *)tmp, '\n')))
 			{
-				ft_strncpy(copy, ptr + 1, ft_strlen(tmp) - (tmp - ptr));
+				ft_strncpy(copy, ptr + 1, ft_strlen((const char *)tmp) - ((const char *)tmp - ptr));
 				push_to_end(rest_cash, copy);
 				*(ptr + 2) = '\0';
-				free(ptr + 2); // ???  // free rest of tmp
+				free(ptr + 2);
 			}
-
-			return tmp;
+			return ((char)tmp);
 		}
-
 		rest_cash = rest_cash->next;
 	}
-
-	return NULL;
+	return ((char)(NULL));
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static list_s * rest_cash; 
-	int		bytes_read;
-	char		*p;
-	char		*tmp;
-	char		buf[BUFF_SIZE + 1];
+	struct list_s	*rest_cash;
+	int				bytes_read;
+	char			*rest;
+	char			*tmp;
+	char			buf[BUFF_SIZE + 1];
 
 	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
-
-	rest =  pop_rest(fd, rest_cash);
-	
+	rest = pop_res(fd, rest_cash);
 	if (rest != NULL && ft_strchr(rest, '\n'))
 	{
 		*line = rest;
-		return;
+		return (0);
 	}
 	else if (rest == NULL)
-	{
 		rest = "";
-	}
-
-	while (bytes_read = read(fd, buf, BUFF_SIZE))
+	while (bytes_read == read(fd, buf, BUFF_SIZE))
 	{
 		buf[bytes_read] = '\0';
-		if ((tmp = ft_strchr(buf, '\n')) // find /0
+		if ((tmp = ft_strchr(buf, '\n')))
 		{
 			*tmp = '\0';
 			if ((tmp - buf) < BUFF_SIZE)
 			{
-				buf = ft_strjoin(rest, buf, tmp-buf);
+				buf = (char *)ft_strjoin(rest, buf);
 				push(rest_cash, tmp + 1);
 			}
 			else
-			{
 				buf = ft_strjoin(rest, buf);
-			}
-			return;
+			return (0);
 		}
-
 	}
-	return ((bytes_read || ft_strlen(residue[fd]) || ft_strlen(*line)) ? 1 : 0);
-}
-
-
-int		get_next_line_v2(const int fd, char **line)
-{
-	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
-		return (-1);
+	return ((bytes_read || ft_strlen((const char *)rest_cash[fd]) || ft_strlen(*line)) ? 1 : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tmyrcell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 15:40:44 by tmyrcell          #+#    #+#             */
-/*   Updated: 2019/10/01 17:21:00 by tmyrcell         ###   ########.fr       */
+/*   Updated: 2019/10/14 17:55:29 by tmyrcell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,23 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <string.h>
 # include "libft/libft.h"
 # include <fcntl.h>
 
 # define BUFF_SIZE 32
+
+struct					list_s
+{
+	void				*data;
+	struct	list_s		*next;
+	struct	list_s		*parent;
+};
+
+struct		rest_data
+{
+	int		fd;
+	char	*rest;
+};
 
 int		get_next_line(const int fd, char **line);
 
